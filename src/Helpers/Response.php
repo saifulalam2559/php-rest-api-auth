f1c3126f48d6c594967166fb0045c06ccbb5722f
Md Saiful Alam <82754863+saifulalam2559@users.noreply.github.com>
2026-01-30 23:13:29 +0100

    Add Response helper class for JSON responses

--- src/Helpers/Response.php
+++ src/Helpers/Response.php
@@ -1,1 +1,13 @@
+<?php
+namespace App\Helpers;
+
+class Response
+{
+    public static function json($data, $status = 200)
+    {
+        http_response_code($status);
+        echo json_encode($data);
+        exit;
+    }
+}
 
